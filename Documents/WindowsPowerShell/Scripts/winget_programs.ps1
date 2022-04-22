c03893614b83dc0e0c392ea8ea7df81bdad5e6d6
mgalvanx <m.galvanx@gmail.com>
2022-04-22 09:43:09 -0700

    update to winget_installation script

--- Documents/WindowsPowerShell/Scripts/winget_programs.ps1
+++ Documents/WindowsPowerShell/Scripts/winget_programs.ps1
@@ -1,6 +1,13 @@
 # Winget Installation Scripts
 
 # https://chrislayers.com/2021/08/01/scripting-winget/
-$app = @(
 
-)
+winget install Microsoft.WindowsTerminal.Preview
+
+winget install Oracle.VirtualBox
+
+winget install Nextcloud.NextcloudDesktop
+
+winget install VMware.WorkstationPro
+
+winget install Mozilla.Firefox
