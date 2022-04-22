e40cbbe6af4dc4ec744552e2096ea983f4406da5
mgalvanx <m.galvanx@gmail.com>
2022-04-21 20:17:45 -0700

    add scripts directory and added Scripts folder to path

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,9 @@
+# Make powershell actually usable for vi mode
 Set-PSReadlineOption -EditMode vi
-get-date
+
+# Adding a module and script folder to $profile
+$scripts = "$(split-path $profile)\Scripts"
+$modules = "$(split-path $profile)\Modules"
+
+# Adding Folder To Path In order to use them everywhere
+$env:path += $scripts
