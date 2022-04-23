347abb63694ce00d500156c521ba310a3f200e03
mgalvanx <m.galvanx@gmail.com>
2022-04-23 08:47:00 -0700

    changes to install

--- Documents/WindowsPowerShell/Scripts/install.ps1
+++ Documents/WindowsPowerShell/Scripts/install.ps1
@@ -1,6 +1,6 @@
 echo "windotfiles" >> .gitignore
 git clone --bare https://gitlab.com/mgalvanx/windotfiles $HOME/windotfiles
-function get-dotfiles { git --git-dir="$HOME\windotfiles" --work-tree="$HOME"}
+function get-dotfiles { git --git-dir="$HOME\windotfiles" --work-tree="$HOME" @Args}
 Set-Alias -Name config -Value get-dotfiles
 config checkout
 config config --local status.showUntrackedFiles no
