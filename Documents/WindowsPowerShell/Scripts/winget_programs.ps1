202b8d3b8dd8ed6acce09b8f03a289fc6a2171c2
mgalvanx <m.galvanx@gmail.com>
2022-05-09 21:08:26 -0700

    temporary fix for winget and microsoft terminal preview

--- Documents/WindowsPowerShell/Scripts/winget_programs.ps1
+++ Documents/WindowsPowerShell/Scripts/winget_programs.ps1
@@ -2,12 +2,14 @@
 
 # https://chrislayers.com/2021/08/01/scripting-winget/
 
-winget install Microsoft.WindowsTerminal.Preview
+winget install microsoft.windowsterminal.preview -v 1.13.10733.0
 
 winget install Oracle.VirtualBox
 
-winget install Nextcloud.NextcloudDesktop
-
-winget install VMware.WorkstationPro
-
 winget install Mozilla.Firefox
+
+winget install vim.vim
+#winget install Nextcloud.NextcloudDesktop
+
+#winget install VMware.WorkstationPro
+
