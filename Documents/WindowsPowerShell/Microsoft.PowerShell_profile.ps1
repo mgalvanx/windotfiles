a47e418f1a4e34d3f47ab08d00c1ca3d52b36df9
mgalvanx <m.galvanx@gmail.com>
2022-05-13 07:56:11 -0700

    Added chocolate path to profile

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -17,3 +17,20 @@ function config {
 function bash {
   & 'C:\Program Files\Git\bin\bash.exe'
 }
+
+# adding vim Function
+#function vi {
+#  & 'C:\Program Files\vim\vim82\vim.exe' @
+#}
+
+# Remove When Winget Add Programs To Path
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+## Remove This as Well
