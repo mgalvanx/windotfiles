fc4f6f3c50136ba267913f22071ad9f9821d4be3
mgalvanx <m.galvanx@gmail.com>
2022-05-13 08:45:30 -0700

    added chocolatey installation script for my programs

---
+++ Documents/WindowsPowerShell/Scripts/chocolatey-programs.ps1
@@ -0,0 +1,16 @@
+# Chocolatey Installation Scripts
+
+
+choco install microsoft-windows-terminal
+
+
+choco install firefox
+
+choco install vim
+
+#choco install virtualbox
+
+#choco install nextcloud-client
+
+#choco install vmwareworkstation
+
